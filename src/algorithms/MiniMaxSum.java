01fe493379d686f9ccf10f041608bdc33fb17fec
Admin <aqilzeka99@gmail.com>
2020-03-30 13:32:25 +0400

    Initial commit

---
+++ src/algorithms/MiniMaxSum.java
@@ -0,0 +1,43 @@
+package algorithms;
+
+import java.util.Scanner;
+
+public class MiniMaxSum {
+    static void miniMaxSum(int[] arr){
+        long MAX = Long.MAX_VALUE;
+        long MIN = Long.MIN_VALUE;
+        long maxSum = 0;
+        long minSum = 0;
+
+        for (int i = 0; i < 5 ; i++) {
+            if (MAX > arr[i]) MAX = arr[i];
+            maxSum += arr[i];
+        }
+
+        for (int i = 0; i < 5; i++) {
+            if (MIN<arr[i]) MIN = arr[i];
+            minSum +=  arr[i];
+        }
+
+        System.out.printf("%d %d",minSum-MIN, maxSum-MAX);
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+        scanner.close();
+    }
+}
---
+++ src/algorithms/PlusMinus.java
@@ -0,0 +1,46 @@
+package algorithms;
+
+import java.util.Scanner;
+
+public class PlusMinus {
+
+    static void plusMinus(int[] arr){
+
+        int size = arr.length;
+        double minus = 0;
+        double plyus = 0;
+        double zero = 0;
+
+        for (int value : arr) {
+            if (value > 0) plyus++;
+            else if (value < 0) minus++;
+            else zero++;
+        }
+
+        System.out.printf("%f%n", plyus/size);
+        System.out.printf("%f%n", minus/size);
+        System.out.printf("%f%n", zero/size);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        plusMinus(arr);
+
+        scanner.close();
+    }
+}
