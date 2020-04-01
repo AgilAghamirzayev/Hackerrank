e56efc6a8483442a24da56c7368bc71a93df69c5
Admin <aqilzeka99@gmail.com>
2020-04-01 21:17:19 +0400

    Solved

---
+++ src/algorithms/TimeConversion.java
@@ -0,0 +1,37 @@
+package algorithms;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+public class TimeConversion {
+
+    static String timeConversion(String s){
+
+        SimpleDateFormat df = new SimpleDateFormat("hh:mm:ssa");
+        SimpleDateFormat outputFormat = new SimpleDateFormat("HH:mm:ss");
+        try {
+            return outputFormat.format(df.parse(s));
+        } catch (ParseException e){
+            return e.toString();
+        }
+
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new FileWriter("timeConvert.txt"));
+        String s = scan.nextLine();
+        String result = timeConversion(s);
+        bw.write(result);
+        bw.newLine();
+        bw.close();
+        scan.close();
+
+
+    }
+}
