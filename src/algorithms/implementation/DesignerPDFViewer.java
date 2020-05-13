ea1571e209ddccdb35dcd049a6ea61a943cee491
Aqil <41694337+Aqilzeka@users.noreply.github.com>
2020-05-13 22:38:47 +0400

    Solved

---
+++ src/algorithms/implementation/DesignerPDFViewer.java
@@ -0,0 +1,13 @@
+package algorithms.implementation;
+
+public class DesignerPDFViewer {
+    static int designerPdfViewer(int[] h, String word) {
+
+        return 9;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/algorithms/implementation/HappyLadybugs.java
@@ -0,0 +1,53 @@
+package algorithms.implementation;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class HappyLadybugs {
+
+    private final static String YES = "YES";
+    private final static String NO = "NO";
+
+    private static boolean hasSingle(String bag) {
+        return bag.codePoints()
+                .filter(c -> c != '_').boxed()
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()))
+                .containsValue(1L);
+    }
+
+    private static String squeeze(final String bag) {
+        return IntStream.range(0,bag.length()).boxed()
+                .flatMap(idx -> {
+                    char curr = bag.charAt(idx);
+                    char prev = idx == 0 ? '_' : bag.charAt(idx-1);
+                    return curr != prev ? Stream.of(String.valueOf(curr)) : Stream.empty();
+                })
+                .collect(Collectors.joining());
+    }
+
+    private static boolean alreadyOrdered(String bag) {
+        String sq = squeeze(bag);
+        return sq.length() == sq.codePoints().distinct().count();
+    }
+
+    static String happyLadybugs(String bag) {
+        return
+                hasSingle(bag)      ? NO :
+                bag.contains("_")   ? YES :
+                alreadyOrdered(bag) ? YES :
+                                      NO;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(happyLadybugs("RBY_YBR"));
+        System.out.println(happyLadybugs("X_Y__X"));
+        System.out.println(happyLadybugs("_"));
+        System.out.println(happyLadybugs("B_RRBR"));
+        System.out.println(happyLadybugs("BBBB"));
+        System.out.println(happyLadybugs("RRGGBBXX"));
+
+    }
+
+}
