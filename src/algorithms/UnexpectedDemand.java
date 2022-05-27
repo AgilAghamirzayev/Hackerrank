6731af1d3b0b8b61b2e076718cbfc7b5358ae755
Aqil <aqilaghamirzayev@gmail.com>
2022-05-27 16:06:33 +0400

    Changed

--- src/algorithms/UnexpectedDemand.java
+++ src/algorithms/UnexpectedDemand.java
@@ -25,4 +25,19 @@ public class UnexpectedDemand {
 
         return count;
     }
+
+    public static int filledfgvfdOrders(List<Integer> order, int k) {
+
+        int widget = k;
+        int count = 0;
+        List<Integer> collect = order.stream().sorted().collect(Collectors.toList());
+        for (Integer o : collect) {
+            if (o <= widget) {
+                count++;
+                widget -= o;
+            }
+        }
+
+        return count;
+    }
 }
